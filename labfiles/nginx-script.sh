db2a4765fc301dce483a7d0ffe0ca473ad1075cf
elle-la <elle@linuxacademy.com>
2020-03-17 08:41:01 -0400

    Nginx from source for Alpine

---
+++ labfiles/nginx-script.sh
@@ -0,0 +1,20 @@
+#! /bin/ash
+
+
+apk --update add openssl-dev pcre-dev zlib-dev wget build-base
+cd /tmp/ && \
+wget http://nginx.org/download/nginx-1.15.1.tar.gz && \
+tar -zxvf nginx-1.15.1.tar.gz 
+cd /tmp/nginx-1.15.1 && \
+./configure \
+    --with-http_ssl_module \
+    --with-http_gzip_static_module \
+    --prefix=/etc/nginx \
+    --http-log-path=/var/log/nginx/access.log \
+    --error-log-path=/var/log/nginx/error.log \
+    --sbin-path=/usr/local/sbin/nginx && \
+make && \
+make install
+apk del build-base
+rm -rf /tmp/nginx-1.15.1
+rm -rf /var/cache/apk/*
